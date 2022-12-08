e0a55db2aa5f95ae97371c53274748fc481aad0f
Dong1333 <mogijisung13@gmail.com>
2022-12-08 12:47:38 +0900

    모든 유효성 검사 적용 완료

--- public/js/index.js
+++ public/js/index.js
@@ -49,13 +49,33 @@ userNameBtn.onclick = () => {
 
 studentCode.onclick = () => {
   const inputStudentCode = prompt('학번을 입력해 주세요');
+  const NumberinputStudentCode = Number(inputStudentCode);
+
+  if (isNaN(inputStudentCode)) {
+    alert('숫자만 입력하여 주세요.');
+    return false;
+  }
+
+  if (NumberinputStudentCode.length != 9) {
+    alert('학번은 9자리를 입력하여 주새요.');
+    return false;
+  }
   localStorage.setItem('inputStudentCode', inputStudentCode);
 
   setStudentCodeInnerHtml(inputStudentCode);
 };
 
 studentEmail.onclick = () => {
-  const inputStudentEmail = prompt('학번을 입력해 주세요');
+  const inputStudentEmail = prompt('email을 입력해 주세요');
+  const exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+
+  // 이메일 형식이 알파벳+숫자@알파벳+숫자.알파벳+숫자 형식이 아닐경우
+
+  if (exptext.test(inputStudentEmail)==false){
+    alert("이메일 형식이 올바르지 않습니다.");
+    return false;
+	};
+
   localStorage.setItem('inputStudentEmail', inputStudentEmail);
 
   setStudentEmailInnerHtml(inputStudentEmail);
